a19839f137f0e2bf090a0f24909e95ad7c937bb4
snowingfox <1503401882@qq.com>
2022-07-06 17:48:25 +0800

    init

--- index.ts
+++ index.ts
@@ -1,17 +1,1 @@
-import express from 'express'
-import config from './src/config/config'
-import { setupServer } from './src/server'
-
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('Express on Vercel')
-})
-
-setupServer(app)
-
-app.listen(config.server.port, async() => {
-  console.log(`Server is running on port ${config.server.port}`)
-})
-
-export default app
+import './app'
--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  server: {
-    port: 8082,
-  },
-}
-
-export default config
